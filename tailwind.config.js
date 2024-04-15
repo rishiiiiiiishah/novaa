d66ce98133c033c7779ed837fcd8edea3c76bba2
rishiiiiiiishah <142864064+rishiiiiiiishah@users.noreply.github.com>
2024-04-15 15:53:10 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    fontFamily: 
+    {
+      'sans': ['Roboto','sans-serif'],
+      'serif': ["Times New Roman","Playfair Display",'serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
